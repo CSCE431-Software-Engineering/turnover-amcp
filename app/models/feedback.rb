0704aca4fdf60c7a3f26a485cc183e14c299d592
root <root@localhost>
2024-02-11 20:37:30 +0000

    Added test cases for feedback, 99.34% covered

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,13 @@
 class Feedback < ApplicationRecord
   validates :feedback, presence: true
+
+  # Set default values before validation if not present
+  before_validation :set_defaults, on: [ :create, :update ]
+
+  private
+  
+  def set_defaults
+    self.name = 'Anonymous' if self.name.blank?
+    self.email = 'N/A' if self.email.blank?
+  end
 end
