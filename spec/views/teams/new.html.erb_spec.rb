5b69539cb4084fd2e15de5574cf21a7a0db2afa3
george-hoyt <root@localhost>
2024-02-12 21:58:30 +0000

    Implemented basic CRUD for teams

---
+++ spec/views/teams/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "teams/new", type: :view do
+  before(:each) do
+    assign(:team, Team.new(
+      team_number: 1,
+      member_count: 1
+    ))
+  end
+
+  it "renders new team form" do
+    render
+
+    assert_select "form[action=?][method=?]", teams_path, "post" do
+
+      assert_select "input[name=?]", "team[team_number]"
+
+      assert_select "input[name=?]", "team[member_count]"
+    end
+  end
+end
