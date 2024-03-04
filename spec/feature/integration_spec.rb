84592e1888d80db74490c32a000f2b2a68e7c7e1
andrewoh365 <andrewoh365@gmail.com>
2024-03-03 19:48:28 -0600

    Updated activities tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -14,24 +14,22 @@ RSpec.describe 'Activity management', type: :feature do
     visit new_activity_path
 
     fill_in 'Event name', with: 'Camp'
-    fill_in 'Start time', with: '06:54:00'
-    fill_in 'End time', with: '17:55:00'
-    fill_in 'Start date', with: '2024-02-05'
-    fill_in 'End date', with: '2024-02-14'
+    fill_in 'Start time', with: '2024-02-05 06:54:00' # Assuming datetime format
+    fill_in 'End time', with: '2024-02-05 17:55:00'   # Assuming datetime format
     fill_in 'Location', with: 'Texas A&M'
     fill_in 'Description', with: 'sdfdfl;fjs'
     fill_in 'Activity type', with: 'Camp'
+    fill_in 'Points', with: '100' # Example value for points
 
     click_button 'Create Activity'
 
     expect(page).to have_content('Activity was successfully created.')
     expect(page).to have_content('Event name: Camp')
-    expect(page).to have_content('Start time: 06:54:00')
-    expect(page).to have_content('End time: 17:55:00')
-    expect(page).to have_content('Start date: 2024-02-05')
-    expect(page).to have_content('End date: 2024-02-14')
+    expect(page).to have_content('Start time: 2024-02-05 06:54:00')
+    expect(page).to have_content('End time: 2024-02-05 17:55:00')
     expect(page).to have_content('Location: Texas A&M')
     expect(page).to have_content('Description: sdfdfl;fjs')
     expect(page).to have_content('Activity type: Camp')
+    expect(page).to have_content('Points: 100')
   end
 end
