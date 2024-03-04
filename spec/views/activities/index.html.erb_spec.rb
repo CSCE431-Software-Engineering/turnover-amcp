f98d1304a90bf83cdee1773677ddce1d8688dab0
andrewoh365 <andrewoh365@gmail.com>
2024-03-04 15:24:47 -0600

    Updated activities index spec

--- spec/views/activities/index.html.erb_spec.rb
+++ spec/views/activities/index.html.erb_spec.rb
@@ -2,41 +2,42 @@
 
 require 'rails_helper'
 
-RSpec.describe 'activities/index', type: :view do
-  before(:each) do
-    assign(:activities, [
-      Activity.create!(
-        event_name: 'Event Name',
-        location: 'Location',
-        description: 'Description',
-        activity_type: 'Activity Type',
-        points: 10,
-        start_time: Time.current,
-        end_time: Time.current + 1.hour
-      ),
-      Activity.create!(
-        event_name: 'Event Name',
-        location: 'Location',
-        description: 'Description',
-        activity_type: 'Activity Type',
-        points: 20,
-        start_time: Time.current,
-        end_time: Time.current + 1.hour
-      )
-    ])
-  end
 
-  it 'renders a list of activities' do
-    render
-    assert_select 'div.activity', count: 2 do |activities|
-      activities.each do |activity|
-        assert_select activity, 'p', text: /Event Name/, count: 1
-        assert_select activity, 'p', text: /Location/, count: 1
-        assert_select activity, 'p', text: /Description/, count: 1
-        assert_select activity, 'p', text: /Activity Type/, count: 1
-        assert_select activity, 'p', text: /Points/, count: 1
-        assert_select activity, 'p', text: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/, count: 2  # Assuming datetime format
-      end
-    end
-  end
+RSpec.describe 'activities/index', type: :view do
+     before(:each) do
+          assign(:activities, [
+               Activity.create!(
+                    event_name: 'Event Name',
+                    location: 'Location',
+                    description: 'Description',
+                    activity_type: 'Activity Type',
+                    points: 10,
+                    start_time: Time.current,
+                    end_time: Time.current + 1.hour
+                    ),
+                    Activity.create!(
+                    event_name: 'Event Name',
+                    location: 'Location',
+                    description: 'Description',
+                    activity_type: 'Activity Type',
+                    points: 20,
+                    start_time: Time.current,
+                    end_time: Time.current + 1.hour
+               )
+          ])
+     end
+
+     it 'renders a list of activities' do
+          render
+          assert_select 'div.activity', count: 2 do |activities|
+                    activities.each do |activity|
+                    assert_select activity, 'p', text: /Event Name/, count: 1
+                    assert_select activity, 'p', text: /Location/, count: 1
+                    assert_select activity, 'p', text: /Description/, count: 1
+                    assert_select activity, 'p', text: /Activity Type/, count: 1
+                    assert_select activity, 'p', text: /Points/, count: 1
+                    assert_select activity, 'p', text: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/, count: 2  # Assuming datetime format
+               end
+          end
+     end
 end
