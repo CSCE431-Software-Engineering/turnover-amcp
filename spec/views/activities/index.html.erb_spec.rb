db2c839c79ab383bf57f5ea44821d2e336bee218
andrewoh365 <andrewoh365@gmail.com>
2024-03-03 20:08:34 -0600

    Updated index spec for activity view

--- spec/views/activities/index.html.erb_spec.rb
+++ spec/views/activities/index.html.erb_spec.rb
@@ -5,38 +5,35 @@ require 'rails_helper'
 RSpec.describe 'activities/index', type: :view do
   before(:each) do
     assign(:activities, [
-             Activity.create!(
-               event_name: 'Event Name',
-               location: 'Location',
-               description: 'Description',
-               activity_type: 'Activity Type',
-               points: 10,
-               start_time: Time.current,
-               end_time: Time.current + 1.hour
-             ),
-             Activity.create!(
-               event_name: 'Event Name',
-               location: 'Location',
-               description: 'Description',
-               activity_type: 'Activity Type',
-               points: 20,
-               start_time: Time.current,
-               end_time: Time.current + 1.hour
-             )
-           ])
+      Activity.create!(
+        event_name: 'Event Name',
+        location: 'Location',
+        description: 'Description',
+        activity_type: 'Activity Type',
+        points: 10,
+        start_time: Time.current,
+        end_time: Time.current + 1.hour
+      ),
+      Activity.create!(
+        event_name: 'Event Name',
+        location: 'Location',
+        description: 'Description',
+        activity_type: 'Activity Type',
+        points: 20,
+        start_time: Time.current,
+        end_time: Time.current + 1.hour
+      )
+    ])
   end
 
   it 'renders a list of activities' do
     render
-    assert_select 'div.activity', count: 2 do |activities|
-      activities.each do |activity|
-        assert_select activity, 'p', text: 'Event Name', count: 1
-        assert_select activity, 'p', text: 'Location', count: 1
-        assert_select activity, 'p', text: 'Description', count: 1
-        assert_select activity, 'p', text: 'Activity Type', count: 1
-        assert_select activity, 'p', text: /\d+ points/, count: 1
-        assert_select activity, 'p', text: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/, count: 2  # Assuming datetime format
-      end
-    end
+    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    assert_select cell_selector, text: Regexp.new('Event Name'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Location'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Description'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Activity Type'.to_s), count: 2
+    assert_select cell_selector, text: /\d+ points/, count: 2
+    assert_select cell_selector, text: /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/, count: 4 # for start_time and end_time
   end
 end
